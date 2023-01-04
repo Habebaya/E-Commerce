d17ad023803e3007236bb6964e164bc7331be105
habebayasser <habebayasser@gmail.com>
2023-01-04 22:12:01 +0200

    implement product_provider.dart

--- lib/business_logic/product_provider.dart
+++ lib/business_logic/product_provider.dart
@@ -4,34 +4,29 @@ import 'dart:developer';
 import 'package:ecommerce/data/repositories/product_repo.dart';
 import 'package:flutter/foundation.dart';
 
+import '../data/models/product_model.dart';
 import '../utility/validator.dart';
 
 class ProductProvider with ChangeNotifier {
-  final ProductRepository _productRepository =
-      ProductRepository();
-
+  final ProductRepository _productRepository = ProductRepository();
+  List<Product> products = [];
 
   Future<void> getAllProduct() async {
     try {
-      final productsResponse =
-      await _productRepository.getAllProducts();
+      final productsResponse = await _productRepository.getAllProducts();
       if (Validators.responseIsValid(productsResponse)) {
         final decoded = jsonDecode(productsResponse.body);
         print(decoded);
 
-        List popularPerson = decoded['results'];
-        for (var element in popularPerson) {
-          // popularPersonList.add(PopularPerson.fromJson(element));
+        List productList = decoded['results'];
+        for (var element in productList) {
+          products.add(Product.fromJson(element));
         }
       }
-
-
     } catch (e) {
       log(e.toString());
     }
 
     notifyListeners();
   }
-
-
 }
