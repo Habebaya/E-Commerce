d26b81f77fc87e602c385364fc5135efb0f6a9b2
habebayasser <habebayasser@gmail.com>
2023-01-05 10:06:37 +0200

    implement getSingleProduct method in Provider

--- lib/business_logic/product_provider.dart
+++ lib/business_logic/product_provider.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 
 import 'package:ecommerce/data/repositories/product_repo.dart';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart';
 
 import '../data/models/product_model.dart';
 import '../utility/validator.dart';
@@ -10,6 +11,7 @@ import '../utility/validator.dart';
 class ProductProvider with ChangeNotifier {
   final ProductRepository _productRepository = ProductRepository();
   List<Product> products = [];
+  late Product product;
 
   Future<void> getAllProduct() async {
     try {
@@ -22,6 +24,24 @@ class ProductProvider with ChangeNotifier {
         for (var element in productList) {
           products.add(Product.fromJson(element));
         }
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+
+    notifyListeners();
+  }
+
+  Future<void> getSingleProduct(int id) async {
+    Response response;
+    try {
+      response = await _productRepository.getSingleProduct(id);
+      if (Validators.responseIsValid(response)) {
+        final decoded = jsonDecode(response.body);
+
+        final singleProduct = decoded;
+        product = Product.fromJson(singleProduct);
+        notifyListeners();
       }
     } catch (e) {
       log(e.toString());
